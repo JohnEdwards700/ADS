9f3aa29bf8604a2cd011aa141114f819fb73fb68
David A. Andrews <andrews@udallas.edu>
2023-10-13 14:53:53 -0500

    Add BST outline

---
+++ Sets/BinarySearchTreeSet.java
@@ -0,0 +1,26 @@
+public class BinarySearchTreeSet implements Set<Integer> {
+    
+    class Tree {
+        Integer e;
+        Tree l;
+        Tree r;
+    }
+
+    Tree myset = null;
+
+    public void add(Integer e) {
+
+    }
+
+    public boolean contains(Integer e) {
+        return false;
+    }
+
+    public void remove(Integer e) {
+        
+    }
+
+    public int length() {
+        return 0;
+    }
+}
