7299f286ff9aeb18375b4db95ed495d926bccd69
David A. Andrews <andrews@udallas.edu>
2023-10-16 14:51:28 -0500

    Add IntegerPair class for example

---
+++ Sets/IntegerPair.java
@@ -0,0 +1,25 @@
+public class IntegerPair implements Comparable<IntegerPair> {
+    Integer f, s;
+
+    public IntegerPair(Integer x, Integer y) {
+        f = x;
+        s = y;
+    }
+
+    public int compareTo(IntegerPair that) {
+        if ( this.f > that.f ) {
+            return 1;
+        }
+        else if ( this.f < that.f ) {
+            return -1;
+        }
+        else if ( this.s > that.s ) {
+            return 1;
+        }
+        else if ( this.s < that.s ) {
+            return -1;
+        }
+
+        return 0;
+    }
+}
