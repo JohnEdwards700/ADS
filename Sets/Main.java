a63bd81080a61d6972c7b629f2bce0da27546287
David A. Andrews <andrews@udallas.edu>
2023-11-01 14:09:05 -0500

    Short wordlist added, and changed to hash

--- Sets/Main.java
+++ Sets/Main.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 public class Main {
 
     public static void main(String[] args) {
-        Set<String> words = new BinarySearchTreeSet<>();
+        Set<String> words = new StringHashSet();
 
         Scanner scan = new Scanner(System.in);
 
