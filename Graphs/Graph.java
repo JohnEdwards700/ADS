30bb2ee3db615ecc651d218a28c17456871bdfd3
David A. Andrews <andrews@udallas.edu>
2023-11-29 14:55:03 -0600

    Add functionality to main and graph

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -4,8 +4,48 @@ import java.util.HashSet;
 public class Graph {
     
     HashMap<Object, HashSet<Object>> graph = new HashMap<>();
+
+    public void addNode(Object n) {
+        HashSet<Object> nSet = graph.get(n);
+
+        if ( nSet == null ) {
+            graph.put(n, new HashSet<Object>());
+        }
+        else {
+            System.out.printf("Node '%s' is already in the graph.\n", n);
+        }
+    }
     
     public void addEdge(Object from, Object to) {
-        graph.get(from).add(to);
+        HashSet<Object> fromSet = graph.get(from);
+
+        if ( fromSet == null ) {
+            System.out.printf("Node '%s' is not in the nodes, so I'm not going to add an edge.\n", from);
+            return;
+        }
+
+        HashSet<Object> toSet = graph.get(to);
+        if ( toSet == null ) {
+            System.out.printf("Node '%s' is not in the nodes, so I'm not going to add an edge.\n", to);
+        }
+
+        fromSet.add(to);
+    }
+
+    public boolean connected(Object from, Object to) {
+        HashSet<Object> fromSet = graph.get(from);
+        HashSet<Object> toSet = graph.get(to);
+
+        if ( fromSet == null ) {
+            System.out.printf("'%s' is not a node.\n", from);
+            return false;
+        }
+
+        if ( toSet == null ) {
+            System.out.printf("'%s' is not a node.\n", to);
+            return false;
+        }
+
+        return fromSet.contains(to);
     }
 }
---
+++ Graphs/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileInputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean done = false;
+        while ( ! done ) {
+            // Get the command
+            String cmdstr = scan.next();
+            String cmd = cmdstr.substring(0,1).toLowerCase();
+            Integer from, to, node;
+
+            switch ( cmd ) {
+                case "c":
+                    from = scan.nextInt();
+                    to = scan.nextInt();
+                    if ( g.connected(from, to) ) {
+                        System.out.printf("'%s' is connected to '%s'\n", from, to);
+                    }
+                    else {
+                        System.out.printf("'%s' is NOT connected to '%s'\n", from, to);
+                    }
+                    break;
+                case "n":
+                    node = scan.nextInt();
+                    g.addNode(node);
+                    break;
+                case "e":
+                    from = scan.nextInt();
+                    to = scan.nextInt();
+                    g.addEdge(from, to);
+                    break;
+                case "q":
+                    done = true;
+                    break;
+                default:
+                    System.err.printf("Unknown command '%s'.\n", cmdstr);
+                    break;
+            }
+            
+        }
+        
+    }
+
+}
